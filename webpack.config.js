757aff76c3681138e714fc679a788fe3c79d7b39
Vladimir Sabantsev <vladimir.sabantsev@gmail.com>
2019-03-07 19:14:27 +0300

    build

--- webpack.config.js
+++ webpack.config.js
@@ -87,7 +87,7 @@ module.exports = (env, argv) => {
       admin: ["@babel/polyfill", "./src/admin/main.js"]
     },
     output: {
-      path: path.resolve(__dirname, "./dist"),
+      path: path.resolve(__dirname, "./docs"),
       filename: "[name].[hash].build.js",
       publicPath: "/",
       chunkFilename: "[chunkhash].js"
