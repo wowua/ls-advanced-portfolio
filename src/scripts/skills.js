5de733eafc3a84dab1a21315824fa158da330451
Vladimir Sabantsev <vladimir.sabantsev@gmail.com>
2019-03-08 13:37:53 +0300

    все компонеты добавлены в мастер

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,1 +1,48 @@
-console.log('this is skills module');
+import Vue from "vue";
+
+const skill = {
+  template: "#skill",
+  props: {
+    skillName: String,
+    skillPercents: Number
+  },
+  methods: {
+    drawCircle() {
+      const circle = this.$refs["color-circle"];
+      const dashOffset = parseInt(
+        getComputedStyle(circle).getPropertyValue("stroke-dasharray")
+      );
+      const percent = (dashOffset / 100) * (100 - this.skillPercents);
+
+      circle.style.strokeDashoffset = percent;
+    }
+  },
+  mounted() {
+    this.drawCircle();
+  }
+};
+
+const skillsRow = {
+  template: "#skills-row",
+  components: {
+    skill
+  },
+  props: {
+    skill: Object
+  }
+};
+
+new Vue({
+  el: "#skills-component",
+  components: {
+    skillsRow
+  },
+  data: {
+    skills: {}
+  },
+  created() {
+    const data = require("../data/skills.json");
+    this.skills = data;
+  },
+  template: "#skills-list"
+});
