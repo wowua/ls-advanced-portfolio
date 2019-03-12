d4c79b210677c9f9643a4e66ec5c6229c5df39ad
Vladimir Sabantsev <vladimir.sabantsev@gmail.com>
2019-03-12 19:51:56 +0300

    добавлен модуль тултипов

--- src/admin/store/index.js
+++ src/admin/store/index.js
@@ -4,9 +4,11 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 import user from './modules/user'
+import skills from './modules/skills'
+import tooltips from './modules/tooltips'
 
 export default new Vuex.Store({
   modules: {
-    user
+    user, skills, tooltips
   }
 });
