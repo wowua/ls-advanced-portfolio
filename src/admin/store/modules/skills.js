204cdd570a3ff757ec0a08405ec8c214d1d8d453
Vladimir Sabantsev <vladimir.sabantsev@gmail.com>
2019-03-13 19:19:48 +0300

    удаление скиллов и групп в сторе

--- src/admin/store/modules/skills.js
+++ src/admin/store/modules/skills.js
@@ -6,7 +6,14 @@ export default {
   },
   mutations: {
     SET_SKILLS_CATEGORIES: (state, skills) => (state.categories = skills),
-    SET_SKILLS_STATE: (state, skills) => (state.skills = skills)
+    ADD_SKILL_CATEGORY: (state, skillCategory) => {
+      state.categories.unshift(skillCategory);
+    },
+    SET_SKILLS_STATE: (state, skills) => (state.skills = skills),
+    ADD_NEW_SKILL: (state, skill) => state.skills.push(skill),
+    REMOVE_SKILL: (state, removedSkillId) => {
+      state.skills = state.skills.filter(skill => skill.id !== removedSkillId);
+    }
   },
   actions: {
     async storeSkillsGroup(store, categoryName) {
@@ -38,9 +45,19 @@ export default {
     },
     async addSkill({ commit }, payload) {
       try {
-        const response = await this.$axios.post('/skills', payload);
+        const response = await this.$axios.post("/skills", payload);
+        commit("ADD_NEW_SKILL", response.data);
+      } catch (error) {
+        throw new Error(error.response.data.message);
+      }
+    },
+    async removeSkill({ commit }, skillId) {
+      try {
+        const response = await this.$axios.delete(`/skills/${skillId}`);
 
-        console.log(response);
+        commit("REMOVE_SKILL", skillId);
+
+        return response;
       } catch (error) {
         throw new Error(error.response.data.message);
       }
