7fb59ee9e4c528a68c53104172488ffddd4a9c33
Vladimir Sabantsev <vladimir.sabantsev@gmail.com>
2019-03-13 19:18:44 +0300

    удаление скиллов

--- src/admin/components/skills-single-item.vue
+++ src/admin/components/skills-single-item.vue
@@ -1,32 +1,71 @@
 <template lang="pug">
-  tr.skills-row-wrapper
+  tr.skills-row-wrapper(
+    :class="{'blocked': blocked}"
+  )
     td.skills__cell {{skill.title}}
     td.skills__cell 
       .skills__input
-        input(:value="skill.percents" type="number").skills__input-text
+        input(:value="skill.percent" type="number").skills__input-text
     td.skills__cell
       iconed-btn(
-        class="is-tick no-words"
+        class="is-pencil no-words grayscale"
       )
     td.skills__cell          
       iconed-btn(
-        class="is-trash no-words"
+        class="is-trash no-words grayscale"
+        @click="removeExistedSkill"
       )
 </template>
 
 <script>
+import { mapActions } from "vuex";
 export default {
   components: {
     iconedBtn: () => import("components/iconed-btn.vue")
   },
+  data() {
+    return {
+      blocked: false
+    }
+  },
   props: {
     skill: Object
+  },
+  methods: {
+    ...mapActions("skills", ["removeSkill"]),
+    ...mapActions("tooltips", ["showTooltip"]),
+    async removeExistedSkill() {
+      this.blocked = true
+      try {
+        const { data } = await this.removeSkill(this.skill.id);
+
+        this.showTooltip({
+          type: 'success',
+          text: data.message
+        })
+
+      } catch (error) {
+        this.showTooltip({
+          type: 'error',
+          text: error.message
+        })
+      } finally {
+        this.blocked = false
+      }
+    }
   }
 };
 </script>
 
 
 <style lang="postcss" scoped>
+.skills-row-wrapper {
+  &.blocked {
+    opacity: .5;
+    pointer-events: none;
+    filter: grayscale(100%);
+  }
+}
 .skills__cell {
   padding-bottom: 4px;
   &:first-child {
